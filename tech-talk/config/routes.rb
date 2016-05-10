5145858a64b8bde9714afd08815aac4d7a82b975
jacobshiff <jacobshiff@gmail.com>
2016-05-10 15:17:32 -0400

    Almost final version

--- tech-talk/config/routes.rb
+++ tech-talk/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :artists
   resources :albums
   get '/', to: 'application#home', as: 'root'
+  get '/grids', to: 'application#grid_demo'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
