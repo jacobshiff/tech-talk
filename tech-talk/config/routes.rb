ac3fe0b53add199438aa5af2847bd6ec06be3d98
jacobshiff <jacobshiff@gmail.com>
2016-05-09 22:03:52 -0400

    Setting up navbar

--- tech-talk/config/routes.rb
+++ tech-talk/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :artists
   resources :artists
   resources :albums
+  get '/', to: 'application#home', as: 'root'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
